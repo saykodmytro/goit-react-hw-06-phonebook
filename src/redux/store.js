0b77f95bf99ef88b6d4eeb28986f781bea4f1b28
Dmytro Sayko <saykodmytro@icloud.com>
2023-11-19 23:00:46 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,6 @@ import { combineReducers, createStore } from 'redux';
 import { contactsReducer } from './contacts.reducer';
 import { filteredReducer } from './filter.reducer';
 
-// Поки що використовуємо редюсер який // тільки повертає отриманий стан
 const rootReducer = combineReducers({
   contactsBook: contactsReducer,
   filteredBook: filteredReducer,
